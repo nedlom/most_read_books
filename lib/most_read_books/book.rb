dd9ad4225393613126053e0b04fcab9d98583cf2
imperative-scientist-4600 <nelom3@gmail.com>
2021-06-03 18:13:23 +0000

    summary & about_author methods updated

--- lib/most_read_books/book.rb
+++ lib/most_read_books/book.rb
@@ -42,41 +42,59 @@ class MostReadBooks::Book
   def publisher
     @publisher = doc.css("#details .row")[1].text.strip.split("by ").last
   end
-
   
-  # def summary
+  def about_author
+    if doc.css(".bookAuthorProfile span").empty?
+      @about_author = "There is no information for this author"
+    else
+      if doc.css(".bookAuthorProfile span").length == 2
+        @about_author = make_author(doc.css(".bookAuthorProfile span")[1])
+      else
+        @about_author = make_author(doc.css(".bookAuthorProfile span")[0])
+      end
+    end
+  end
+
+  def summary
+    @summary = make_paragraphs(doc.css("#description span")[1])
+    binding.pry
+  end
+  
+  def make_paragraphs(element)
+    text_array = element.children.map do |node|
+      node.text
+    end
+    
+    # text_groups is array of form [[true/false, [strings]],...]
+    # true/false, strings grouped together based on return value of block
+    text_groups = text_array.chunk do |line|
+      line != "" && line != " "
+    end.to_a
+    
+    text_groups.map do |group|
+      group[1].join if group[0]
+    end.compact
+  end
+  
+  def format_summary
+    self.summary.each do |p|
+      puts p.scan(/(.{1,75})(?:\s|$)/m)
+      puts ""
+    end
+  end
+  
+  def format_author(array)
+    array.each do |p|
+      puts p.scan(/(.{1,75})(?:\s|$)/m)
+      puts ""
+    end
+  end
+  
+    # def summary
   #   @summary = doc.css("#description span")[1].text
   # end
   
-  def about_author
-    if !doc.css(".bookAuthorProfile span").empty?
-      if doc.css(".bookAuthorProfile span").length == 2
-        about_author = doc.css(".bookAuthorProfile span")[1]
-      else
-        about_author = doc.css(".bookAuthorProfile span")[0]
-      end
-    else
-      about_author = "No author info"
-    end
-    @about_author = about_author
-    binding.pry
-  end
-    
-
-    # if this has 2 elements choose [1]
-    # if no span then no profile
-    # puts doc.css(".bookAuthorProfile").text
-    # binding.pry
-    # element = doc.css(".bookAuthorProfile span")[1]
-    # if !element.nil?
-    #   @about_author = text_array(element)
-    # else
-    #   @about_author = ["There is no info about this author."]
-    # end
-  # end
-  
-  def summary
-    # paragraphs = doc.css("#description span")[1].children.map do |p|
+     # paragraphs = doc.css("#description span")[1].children.map do |p|
     #   p.text
     # end
 
@@ -91,57 +109,33 @@ class MostReadBooks::Book
     # @summary = summary.select do |p|
     #   p != "" && p != " "
     # end
-    
-    text_array = doc.css("#description span")[1].children.map do |node|
-      node.text
-    end
-    
-    # text_groups is array of form [[true/false, [strings]],...]
-    # true/false, strings grouped together based on return value of block
-    text_groups = text_array.chunk do |line|
-      line != "" && line != " "
-    end.to_a
-    
-    @summary = text_groups.map do |group|
-      group[1].join if group[0]
-    end.compact
-  end
   
-  def maker
-    text_array = doc.css("#description span")[1].children.map do |node|
-      node.text
-    end
-    
-    # text_groups is array of form [[true/false, [strings]],...]
-    # true/false, strings grouped together based on return value of block
-    text_groups = text_array.chunk do |line|
-      line != "" && line != " "
-    end.to_a
-    
-    @summary = text_groups.map do |group|
-      group[1].join if group[0]
-    end.compact
-  end
+      
+
+    # if this has 2 elements choose [1]
+    # if no span then no profile
+    # puts doc.css(".bookAuthorProfile").text
+    # binding.pry
+    # element = doc.css(".bookAuthorProfile span")[1]
+    # if !element.nil?
+    #   @about_author = text_array(element)
+    # else
+    #   @about_author = ["There is no info about this author."]
+    # end
+  # end
   
-  def format_summary
-    self.summary.each do |p|
-      puts p.scan(/(.{1,75})(?:\s|$)/m)
-      puts ""
-    end
-  end
-  
-  def text_array(element)
-    node_set = element.children
-      paragraphs = node_set.map do |n|
-        if n.name == "br"
-          n.name
-        else
-          n.text
-        end
-      end
-      paragraphs.delete(" ")
-      paragraphs = paragraphs.join.split("brbr")
-      paragraphs.delete_if {|p| p == "" || p == " " || p == "br"}
-      paragraphs
-  end
+  # def text_array(element)
+  #   node_set = element.children
+  #     paragraphs = node_set.map do |n|
+  #       if n.name == "br"
+  #         n.name
+  #       else
+  #         n.text
+  #       end
+  #     end
+  #     paragraphs.delete(" ")
+  #     paragraphs = paragraphs.join.split("brbr")
+  #     paragraphs.delete_if {|p| p == "" || p == " " || p == "br"}
+  #     paragraphs
+  # end
 end
