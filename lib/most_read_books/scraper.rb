969bbda8d7c799ac157ea4ab39afaa99b770a2ba
imperative-scientist-4600 <nelom3@gmail.com>
2021-06-09 19:30:45 +0000

    scrape_books updated

--- lib/most_read_books/scraper.rb
+++ lib/most_read_books/scraper.rb
@@ -3,11 +3,11 @@ class MostReadBooks::Scraper
   def scrape_books
     page = Nokogiri::HTML(open("https://www.goodreads.com/book/most_read").read)
     page.css("tr").each do |b|
-      title = b.css("[itemprop='name']")[0].text
-      author = b.css("[itemprop='name']")[1].text
-      url = "https://www.goodreads.com/#{b.css(".bookTitle")[0]['href']}"
+      title = b.css("[itemprop='name']").first.text
+      author = b.css("[itemprop='name']").last.text
+      url = "https://www.goodreads.com/#{b.css(".bookTitle").first['href']}"
       ratings = b.css(".minirating").text.strip
-      readers = b.css(".greyText.statistic").text.strip.split("\n")[0]
+      readers = b.css(".greyText.statistic").text.strip.split("\n").first
       MostReadBooks::Book.new(title, author, url, ratings, readers)
     end
   end
